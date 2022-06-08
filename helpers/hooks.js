c192c92d2379b7a37b25f5dbe5a16a90edc92c32
Max_K <kavokinm@gmail.com>
2022-06-08 20:41:56 +0300

    Hide overflow during page transition.

--- helpers/hooks.js
+++ helpers/hooks.js
@@ -1,25 +1,6 @@
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 
-const routes = [{ path: '/' }, { path: '/about' }, { path: '/testpage' }];
-
-export const useDir = () => {
-  const { pathname, query } = useRouter();
-
-  const { from = pathname } = query;
-
-  const pathname_i = routes.findIndex((el) => el.path === pathname);
-  const from_i = routes.findIndex((el) => el.path === from);
-
-  const diff = pathname_i - from_i;
-
-  let dir = null;
-  if (diff < 0) dir = 'left';
-  if (diff > 0) dir = 'right';
-
-  return { dir, pathname };
-};
-
 export const useStateWithRef = (initValue) => {
   const [value, setValue] = useState(initValue);
   const valueRef = useRef(value);
--- pages/about.js
+++ pages/about.js
@@ -5,19 +5,23 @@ import { useEffect, useState } from 'react';
 export default function About({ dir }) {
   return (
     <section className={`section about ${dir}`}>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur maxime
-        alias sequi corrupti in, veniam eum nulla. Repudiandae, assumenda incidunt nemo
-        quia nobis fugit libero ipsam harum consequatur quidem amet quisquam facere at
-        inventore quod similique cumque dolor ab ducimus accusamus id! Necessitatibus
-        beatae voluptas eaque assumenda in exercitationem voluptatibus. In nihil nemo
-        laborum eveniet iusto eum et. Cumque, numquam voluptatem! Dignissimos quas porro
-        quia suscipit quisquam soluta repudiandae, maxime tenetur tempora, delectus, cum
-        saepe velit doloremque. Nihil repellat, quos alias a est quae cum. Sit, laborum?
-        Dolorum sunt alias accusantium, doloribus earum corrupti rem fugiat facilis veniam
-        tempore quo?
-      </p>
+      <div className='section__frame'>
+        <div className='about__block'>
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur maxime
+            alias sequi corrupti in, veniam eum nulla. Repudiandae, assumenda incidunt
+            nemo quia nobis fugit libero ipsam harum consequatur quidem amet quisquam
+            facere at inventore quod similique cumque dolor ab ducimus accusamus id!
+            Necessitatibus beatae voluptas eaque assumenda in exercitationem voluptatibus.
+            In nihil nemo laborum eveniet iusto eum et. Cumque, numquam voluptatem!
+            Dignissimos quas porro quia suscipit quisquam soluta repudiandae, maxime
+            tenetur tempora, delectus, cum saepe velit doloremque. Nihil repellat, quos
+            alias a est quae cum. Sit, laborum? Dolorum sunt alias accusantium, doloribus
+            earum corrupti rem fugiat facilis veniam tempore quo?
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
