c502ad146041c80867962b092c5b184dbbbcb29d
Max_K <kavokinm@gmail.com>
2022-06-15 20:24:07 +0300

    Styling arrows navigation.

--- components/Arrows.js
+++ components/Arrows.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { getPath } from '../helpers/functions';
 import { CSSTransition } from 'react-transition-group';
@@ -7,9 +8,7 @@ const Arrows = ({ onExited, active }) => {
   const router = useRouter();
   const { pathname } = router;
 
-  const onClickNext = () => router.push(getPath(pathname, 1));
-
-  const onClickPrev = () => router.push(getPath(pathname, -1));
+  /* const [value, setValue] = useState(false); */
 
   const timeout = 500;
   return (
@@ -21,12 +20,20 @@ const Arrows = ({ onExited, active }) => {
         onExited={onExited}
       >
         <div className='arrow__right'>
-          <button onClick={onClickNext}>Next</button>
+          <Link href={getPath(pathname, 1)}>
+            <a className='arrow__link' />
+          </Link>
+          <span className='arrow__text'>next</span>
+          <div className='arrow__arrow' />
         </div>
       </CSSTransition>
       <CSSTransition classNames={'arrow__left'} timeout={timeout} in={active}>
         <div className='arrow__left'>
-          <button onClick={onClickPrev}>Prev</button>
+          <Link href={getPath(pathname, -1)}>
+            <a className='arrow__link' />
+          </Link>
+          <div className='arrow__arrow' />
+          <span className='arrow__text'>prev</span>
         </div>
       </CSSTransition>
     </>
