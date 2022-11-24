ed1a5636e0cff454f08eb02af9db118f324dcc25
Max_K <kavokinm@gmail.com>
2022-11-24 23:03:22 +0300

    Working on falling dots

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,49 +5,92 @@ import Section, { Props } from '../components/SectionSlider/Section';
 import SideLinks from '../components/SideLinks';
 import TypeWriterText from '../components/TypeWriterText';
 
-let prevTime = 0;
-let framesPerSecond = 60;
-let oneFrameTime = 1000 / framesPerSecond;
-
-let x = 200;
-let y = 10;
-
-let xSize = 3;
-let ySize = 3;
-
-let accelX = 0;
-let accelY = 0.2;
-
-let speedX = 3;
-let speedY = 0;
-
-let maxSpeedX = 5;
-let maxSpeedY = 20;
-
-const barrier = {
-  x: 305,
-  y: 700,
-  height: 50,
-  width: 300,
-};
-
-interface IPoint {
+interface IPos {
   x: number;
   y: number;
 }
 
 interface ILine {
-  a: IPoint;
-  b: IPoint;
+  a: IPos;
+  b: IPos;
 }
 
-interface IRect extends IPoint {
+interface IRect extends IPos {
   h: number;
   w: number;
 }
 
+interface IDot extends IRect {
+  speedX: number;
+  speedY: number;
+  color: string;
+}
+
+type TDotArr = IDot[];
+
+const framesPerSecond = 60;
+const oneFrameTime = 1000 / framesPerSecond;
+let prevTime = 0;
+
+const accelX = 0;
+const accelY = 0.2;
+
+const maxSpeedX = 5;
+const maxSpeedY = 20;
+
+const minBounceSpeedX = 2.5;
+const minBounceSpeedY = 2.5;
+
+const barrier: IRect = {
+  x: 305,
+  y: 700,
+  w: 300,
+  h: 50,
+};
+
+const dot: IDot = {
+  x: 200,
+  y: 10,
+  w: 3,
+  h: 3,
+  speedX: 3,
+  speedY: 0,
+  color: 'white',
+};
+
+const dotArr: TDotArr = [
+  {
+    x: 200,
+    y: 10,
+    w: 3,
+    h: 3,
+    speedX: 3,
+    speedY: 0,
+    color: 'white',
+  },
+  {
+    x: 200,
+    y: 10,
+    w: 3,
+    h: 3,
+    speedX: 4,
+    speedY: 0,
+    color: 'red',
+  },
+  {
+    x: 200,
+    y: 500,
+    w: 3,
+    h: 3,
+    speedX: 3,
+    speedY: 0,
+    color: 'yellow',
+  },
+];
+
+type TDir = 'top' | 'left' | 'right' | 'bottom';
+
 const lineLineCol = (l1: ILine, l2: ILine) => {
-  // calculate the direction of the lines=
   const uA =
     ((l2.b.x - l2.a.x) * (l1.a.y - l2.a.y) - (l2.b.y - l2.a.y) * (l1.a.x - l2.a.x)) /
     ((l2.b.y - l2.a.y) * (l1.b.x - l1.a.x) - (l2.b.x - l2.a.x) * (l1.b.y - l1.a.y));
@@ -55,29 +98,19 @@ const lineLineCol = (l1: ILine, l2: ILine) => {
     ((l1.b.x - l1.a.x) * (l1.a.y - l2.a.y) - (l1.b.y - l1.a.y) * (l1.a.x - l2.a.x)) /
     ((l2.b.y - l2.a.y) * (l1.b.x - l1.a.x) - (l2.b.x - l2.a.x) * (l1.b.y - l1.a.y));
 
-  // if uA and uB are between 0-1, lines are colliding
+  let res: IPos;
   if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
-    // optionally, draw a circle where the lines meet
-    const intrX = l1.a.x + uA * (l1.b.x - l1.a.x);
-    const intrY = l1.a.y + uA * (l1.b.y - l1.a.y);
+    const x = l1.a.x + uA * (l1.b.x - l1.a.x);
+    const y = l1.a.y + uA * (l1.b.y - l1.a.y);
 
-    return { intrX, intrY };
+    res = { x, y };
   }
-  return false;
+
+  return res;
 };
 
-console.log(
-  lineLineCol(
-    { a: { x: 1, y: 6 }, b: { x: 10, y: 6 } },
-    { a: { x: 2, y: 9 }, b: { x: 7, y: 4 } }
-  )
-);
-
 const lineRectCol = (l: ILine, r: IRect) => {
-  // check if the line has hit any of the rectangle's sides
-  // uses the Line/Line function below
-
-  const left = lineLineCol(l, { a: { x: r.x, y: r.y }, b: { x: r.y, y: r.y + r.h } });
+  const left = lineLineCol(l, { a: { x: r.x, y: r.y }, b: { x: r.x, y: r.y + r.h } });
   const right = lineLineCol(l, {
     a: { x: r.x + r.w, y: r.y },
     b: { x: r.x + r.w, y: r.y + r.h },
@@ -88,25 +121,14 @@ const lineRectCol = (l: ILine, r: IRect) => {
     b: { x: r.x + r.w, y: r.y + r.h },
   });
 
-  /* const left = lineLineCol(l, r.x, r.y, r.x, r.y + r.h);
-  const r.ight = lineLineCol(l, r.x + r.w, r.y, r.x + r.w, r.y + r.h);
-  const top = lineLineCol(l, r.x, r.y, r.x + r.w, r.y);
-  const bottom = lineLineCol(l, r.x, r.y + r.h, r.x + r.w, r.y + r.h);
- */
+  let res: { pos: IPos; type: TDir };
 
-  /* const left = lineLineCol(l, rx, ry, rx, ry + rh);
-  const right = lineLineCol(l, rx + rw, ry, rx + rw, ry + rh);
-  const top = lineLineCol(l, rx, ry, rx + rw, ry);
-  const bottom = lineLineCol(l, rx, ry + rh, rx + rw, ry + rh);
- */
-  // if ANY of the above are true, the line
-  // has hit the rectangle
-  console.log({ left, right, top, bottom });
+  if (top) res = { type: 'top', pos: top };
+  if (left) res = { type: 'left', pos: left };
+  if (right) res = { type: 'right', pos: right };
+  if (bottom) res = { type: 'bottom', pos: bottom };
 
-  if (left || right || top || bottom) {
-    return true;
-  }
-  return false;
+  return res;
 };
 
 const About: FC<Props> = (props) => {
@@ -120,30 +142,50 @@ const About: FC<Props> = (props) => {
     const diff = time - prevTime;
     const isFrame = diff > oneFrameTime;
 
-    if (isFrame && canvasRef.current.height > y && canvasRef.current.width > x) {
-      prevTime = time;
-      // ctx.current.clearRect(x, y, xSize, ySize);
-      // ctx.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    const moveDot = (dot: IDot) => {
+      if (
+        isFrame &&
+        canvasRef.current.height > dot.y &&
+        canvasRef.current.width > dot.x
+      ) {
+        prevTime = time;
+        // ctx.current.clearRect(x, y, xSize, ySize);
+        // ctx.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 
-      ctx.current.fillStyle = 'white';
+        ctx.current.fillStyle = dot.color;
 
-      speedX = Math.min(speedX + accelX, maxSpeedX);
-      speedY = Math.min(speedY + accelY, maxSpeedY);
+        dot.speedX = Math.min(dot.speedX + accelX, maxSpeedX);
+        dot.speedY = Math.min(dot.speedY + accelY, maxSpeedY);
 
-      let newX = x + speedX;
-      let newY = y + speedY;
+        let newX = dot.x + dot.speedX;
+        let newY = dot.y + dot.speedY;
 
-      let inX = newX + xSize > barrier.x && newX < barrier.x + barrier.width;
-      let inY = newY + ySize > barrier.y && newY < barrier.y + barrier.height;
-      let isCollision = inX && inY;
+        const line: ILine = { a: { x: dot.x, y: dot.y }, b: { x: newX, y: newY } };
 
-      console.log({ inX, inY, isCollision });
+        const col = lineRectCol(line, barrier);
 
-      x = newX;
-      y = newY;
+        if (dot.color === 'yellow' && col) {
+          console.log({ col, barrier, line });
+        }
 
-      ctx.current.fillRect(x, y, xSize, ySize);
-    }
+        if (col) {
+          const { type, pos } = col;
+
+          if (type === 'top') {
+            dot.speedY = Math.min(-dot.speedY * 0.25, -minBounceSpeedY);
+            newX = pos.x;
+            newY = pos.y - 0.1;
+          }
+        }
+
+        dot.x = newX;
+        dot.y = newY;
+
+        ctx.current.fillRect(dot.x - dot.w / 2, dot.y - dot.h / 2, dot.w, dot.h);
+      }
+    };
+
+    dotArr.forEach(moveDot);
 
     requestRef.current = requestAnimationFrame(animate);
   };
@@ -160,9 +202,9 @@ const About: FC<Props> = (props) => {
 
     setCanvasSize();
 
-    const { x, y, height, width } = barrier;
+    const { x, y, w, h } = barrier;
     ctx.current.fillStyle = 'red';
-    ctx.current.fillRect(x, y, width, height);
+    ctx.current.fillRect(x, y, w, h);
 
     window.addEventListener('resize', setCanvasSize);
 
